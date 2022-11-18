e9b43721a69dc109836bc3bd88009dfe8db96d90
mlao <manolinlao@gmail.com>
2022-11-18 10:20:39 +0100

    headers

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from './product.model';
@@ -15,6 +15,38 @@ export class RestDataSource {
   ) {}
 
   getData(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url);
+    //return this.http.get<Product[]>(this.url);
+    return this.sendRequest<Product[]>('GET', this.url);
+  }
+
+  saveProduct(product: Product): Observable<Product> {
+    //return this.http.post<Product>(this.url, product);
+    return this.sendRequest<Product>('POST', this.url, product);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    //return this.http.put<Product>(`${this.url}/${product.id}`, product);
+    return this.sendRequest<Product>(
+      'PUT',
+      `${this.url}/${product.id}`,
+      product
+    );
+  }
+
+  deleteProduct(id: number): Observable<Product> {
+    //return this.http.delete<Product>(`${this.url}/${id}`);
+    return this.sendRequest<Product>('DELETE', `${this.url}/${id}`);
+  }
+
+  private sendRequest<T>(
+    verb: string,
+    url: string,
+    body?: Product
+  ): Observable<T> {
+    let myHeaders = new HttpHeaders();
+    myHeaders = myHeaders.set('Access-Key', '<secret>');
+    myHeaders = myHeaders.set('Applition-Names', ['exampleApp', 'proAngular']);
+
+    return this.http.request<T>(verb, url, { body: body, headers: myHeaders });
   }
 }
